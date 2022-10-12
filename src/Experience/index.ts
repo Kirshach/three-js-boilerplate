35e5ac1f501bd856db82fbd7c822dd8740d428df
kirshach <kirshach@mail.ru>
2022-10-13 02:29:39 +0400

    get rid of ThreeJsServices class

--- src/Experience/index.ts
+++ src/Experience/index.ts
@@ -1,7 +1,65 @@
-import ThreeJsServices from '../three-js-services';
+import Resources, { ResourcesEvents } from '../utils/Resources';
+import Time, { TimeEvents } from '../utils/Time';
+
+import {
+  Camera,
+  Config,
+  ConfigEvents,
+  ConfigEventPayload,
+  Renderer,
+  World,
+  Scene,
+  Canvas,
+  Environment
+} from '../three-js-services';
 
 class Experience {
-  private services = new ThreeJsServices();
+  public config: Config;
+  public scene: Scene;
+  public world: World;
+  public time: Time;
+  public resources: Resources;
+  public canvas: Canvas;
+  public camera: Camera;
+  public environment: Environment;
+  public renderer: Renderer;
+
+  constructor() {
+    // non-dependant classes
+    this.config = new Config();
+    this.canvas = new Canvas();
+    this.scene = new Scene();
+    this.time = new Time();
+    this.resources = new Resources();
+
+    // dependant classes
+    this.camera = new Camera(this.scene, this.canvas, this.config);
+    this.environment = new Environment(this.resources, this.scene);
+    this.renderer = new Renderer(this.config, this.scene, this.camera, this.canvas);
+    this.world = new World(this.scene, this.resources);
+
+    // event listeners
+    this.config.subscribe(ConfigEvents.RESIZE, this.handleResize);
+    this.time.subscribe(TimeEvents.TICK, this.handleTick);
+    this.resources.subscribe(ResourcesEvents.FINISH_LOADING, this.environment.initialize);
+  }
+
+  handleResize = (payload: ConfigEventPayload) => {
+    this.camera.handleResize(payload);
+    this.renderer.handleResize(payload);
+  }
+
+  handleTick = () => {
+    this.camera.update();
+    this.renderer.render();
+  }
+
+  destroy = () => {
+    this.time.destroy();
+    this.config.destroy();
+    this.scene.clear(); // anything else to clear up the scene?
+    this.camera.destroy();
+  }
 }
 
 export default Experience;
--- src/three-js-services/index.ts
+++ src/three-js-services/index.ts
@@ -1,61 +1,9 @@
-import Resources, { ResourcesEvents } from '../utils/Resources';
-import Time, { TimeEvents } from '../utils/Time';
+export { default as Camera } from './camera';
+export { default as Config, ConfigEvents, } from './config';
+export type { ConfigEventPayload } from './config';
+export { default as Renderer } from './renderer';
+export { default as World } from './world';
+export { default as Scene } from './scene';
+export { default as Canvas } from './canvas';
+export { default as Environment } from './environment';
 
-import Camera from './camera';
-import Config, { ConfigEvents, ConfigEventPayload } from './config';
-import Renderer from './renderer';
-import World from './world';
-import Scene from './scene';
-import Canvas from './canvas';
-import Environment from './environment';
-
-class ThreeJsServices {
-  public config: Config;
-  public scene: Scene;
-  public world: World;
-  public time: Time;
-  public resources: Resources;
-  public canvas: Canvas;
-  public camera: Camera;
-  public environment: Environment;
-  public renderer: Renderer;
-
-  constructor() {
-    // non-dependant classes
-    this.config = new Config();
-    this.canvas = new Canvas();
-    this.scene = new Scene();
-    this.time = new Time();
-    this.resources = new Resources();
-
-    // dependant classes
-    this.camera = new Camera(this.scene, this.canvas, this.config);
-    this.environment = new Environment(this.resources, this.scene);
-    this.renderer = new Renderer(this.config, this.scene, this.camera, this.canvas);
-    this.world = new World(this.scene, this.resources);
-
-    // event listeners
-    this.config.subscribe(ConfigEvents.RESIZE, this.handleResize);
-    this.time.subscribe(TimeEvents.TICK, this.handleTick);
-    this.resources.subscribe(ResourcesEvents.FINISH_LOADING, this.environment.initialize);
-  }
-
-  handleResize = (payload: ConfigEventPayload) => {
-    this.camera.handleResize(payload);
-    this.renderer.handleResize(payload);
-  }
-
-  handleTick = () => {
-    this.camera.update();
-    this.renderer.render();
-  }
-
-  destroy = () => {
-    this.time.destroy();
-    this.config.destroy();
-    this.scene.clear(); // anything else to clear up the scene?
-    this.camera.destroy();
-  }
-}
-
-export default ThreeJsServices;
