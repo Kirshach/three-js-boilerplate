94fec81606a305a811cce433b32dc7f8bb53d8bf
kirshach <kirshach@mail.ru>
2022-12-04 19:17:11 +0400

    fix eslint config

--- eslintrc.js
+++ eslintrc.js
@@ -1,11 +1,41 @@
 module.exports = {
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/eslint-recommended', 'prettier'],
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:import/recommended',
+    'plugin:import/typescript',
+    'prettier',
+  ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 'latest',
   },
-  plugins: ['@typescript-eslint'],
+  plugins: ['import', '@typescript-eslint'],
+  rules: {},
   rules: {
+    indent: ['error', 2],
+    'linebreak-style': ['error', 'unix'],
+    quotes: ['error', 'single'],
+    semi: ['error', 'always'],
     '@typescript-eslint/consistent-type-imports': 'error',
+  },
+  settings: {
+    'import/resolver': {
+      alias: {
+        map: [
+          ['#components', './src/components'],
+          ['#hooks', './src/hooks'],
+          ['#store', './src/store'],
+          ['#stubs', './src/stubs'],
+          ['#const', './src/const'],
+          ['#types', './src/types'],
+        ],
+        extensions: ['.ts', '.tsx', '.json'],
+      },
+    },
   },
 };
